2fda54def5d6e0a2756b54441ae3d2885d4f1fb6
garu2 <elgafo.nakama@gmail.com>
2022-07-17 20:05:46 -0400

    FIX: remove comment styles

--- custom.js
+++ custom.js
@@ -40,7 +40,7 @@ function deleteProduct(e) {
         
         countProduct--;
     }
-    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos  
+    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
     if (buyThings.length === 0) {
         priceTotal.innerHTML = 0;
         amountProduct.innerHTML = 0;
